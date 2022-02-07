9d11be542937ebafe4c6b77c5690c41ced8e0c1d
ccx1010sky <ccx1010@gmail.com>
2022-02-07 14:45:42 +0000

    testAdd() pass

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+    @Before
+    public void before(){ calculator = new Calculator(3,5);};
+
+    @Test
+    public void testAdd() {
+        assertEquals(8, calculator.add());
+    }
 
 
 
