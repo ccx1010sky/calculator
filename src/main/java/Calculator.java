02dfcbce32613a54a8c51e7e9f4a03cfce3c4a00
ccx1010sky <ccx1010@gmail.com>
2022-02-07 14:44:38 +0000

    create Calculator.java and CalculatorTest.java

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    private int num1;
+    private int num2;
+
+    public Calculator(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public int add(){
+        return this.num1 + this.num2;
+    }
+
+    public int sub(){
+        return this.num1 - this.num2;
+    }
+
+    public int multi(){
+        return this.num1 * this.num2;
+    }
+
+    public double divide(){
+        return this.num1 / this.num2;
+    }
+}
